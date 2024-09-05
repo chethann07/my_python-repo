3cf6c21acec80ab1d5aab8c14e6763238353cd51
Chethan <gchetan8008@gmail.com>
2024-09-05 10:36:59 +0530

    python programs

--- Programs/input.py
+++ Programs/input.py
@@ -1,1 +1,16 @@
 # input() -> prompts the user to enter the data
+
+name = input("Enter your name :")
+age = int(input("Enter your age :"))
+
+print(f"Your name is {name} and you are {age} years old")
+
+#Exercise 1 : Calculating the area of a rectangle
+
+#formula : area = length * width
+
+length = float(input("Enter the length :"))
+width = float(input("Enter the width :"))
+area = length * width
+
+print(f"The area of the rectangle is {area}")
